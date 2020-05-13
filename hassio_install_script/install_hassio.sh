74a11c2a476887ecea86b68cc8d27b224c31b930
Marco <marco@marcolino.net>
2020-05-13 01:03:24 +0100

    Update install_hassio.sh

--- hassio_install_script/install_hassio.sh
+++ hassio_install_script/install_hassio.sh
@@ -4,24 +4,24 @@
  
  
 # Install dependencies
-echo ""; echo "Step 1, Installing dependencies"; echo "";
+tput setaf 201; echo ""; echo "Step 1, Installing dependencies"; echo "";
 sudo apt-get install bash jq curl avahi-daemon dbus software-properties-common apparmor-utils -y;  # auto-answer Yes
 
  
 # Install docker
-echo ""; echo "Step 2, Installing Docker"; echo "";
-echo ""; echo "Step 2a"; echo "";
+tput setaf 201; echo ""; echo "Step 2, Installing Docker"; echo "";
+tput setaf 201; echo ""; echo "Step 2a"; echo "";
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y;  # auto-answer Yes
-echo ""; echo "Step 2b"; echo "";
+tput setaf 201; echo ""; echo "Step 2b"; echo "";
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -;
-echo ""; echo "Step 2c"; echo "";
+tput setaf 201; echo ""; echo "Step 2c"; echo "";
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable";
-echo ""; echo "Step 2d"; echo "";
+tput setaf 201; echo ""; echo "Step 2d"; echo "";
 sudo apt-get update;
-echo ""; echo "Step 2e"; echo "";
+tput setaf 201; echo ""; echo "Step 2e"; echo "";
 sudo apt-get install docker-ce -y;  # auto-answer Yes
  
  
 # Replace user path with your user
-echo ""; echo "Step 3, Installing Home Assistant"; echo "";
+tput setaf 201; echo ""; echo "Step 3, Installing Home Assistant"; echo "";
 curl -sL https://raw.githubusercontent.com/home-assistant/supervised-installer/master/installer.sh | bash -s -- -d /home/me/docker/hassio
